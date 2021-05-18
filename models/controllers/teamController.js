8209c17356fd3d3d3a5c69b027c3315d7b026196
kwon, minho <minhob38@gmail.com>
2021-05-19 03:13:31 +0900

    Refactor team router and team controller

--- models/controllers/teamController.js
+++ models/controllers/teamController.js
@@ -1,5 +1,61 @@
+const Location = require("../Location");
+const Sports = require("../Sports");
 const Team = require("../Team");
 const User = require("../User");
+
+const getTeams = async (province, city, district, sports) => {
+  const [location, _sports] = await Promise.all([
+    await Location.findOne({ province, city, district }),
+    await Sports.findOne({ sports }),
+  ]);
+
+  const locationOid = location["_id"];
+  const sportsOid = _sports["_id"];
+
+  const teams = await Team.find({
+    $and: [{ location: locationOid }, { sports: sportsOid }],
+  }).populate("location")
+    .populate("caption")
+    .populate("sports")
+    .populate("members")
+    .populate({ path: "matches", populate: { path: "playground" } })
+    .populate({ path: "matches", populate: { path: "teams" } });
+
+  const _teams = teams.map((team) => {
+    const matches = team.matches.map((match) => {
+      const { playtime, playground } = match;
+
+      return {
+        start: playtime.start,
+        end: playtime.end,
+        playgroundName: playground.name,
+        province: playground.address.province,
+        city: playground.address.city,
+        district: playground.address.district,
+        teams: match.teams[1]
+          ? [match.teams[0].name, match.teams[1].name]
+          : [match.teams[0].name],
+      };
+    });
+
+    return {
+      id: team["_id"],
+      name: team.name,
+      captin: team.captin.name,
+      province: team.location.province,
+      city: team.location.city,
+      district: team.location.district,
+      emblem: team.emblem,
+      members: team.members,
+      manner: team.repute.manner,
+      ability: team.repute.ability,
+      matches: matches,
+      rank: team.rank,
+    };
+  });
+
+  return _teams;
+};
 
 const createTeam = async (email, name, sports, location, imageS3) => {
   const _user = await User.findOne({ email }, "_id teams");
@@ -16,24 +72,33 @@ const createTeam = async (email, name, sports, location, imageS3) => {
 
   _user.teams.push(_team["_id"]);
   await _user.save();
+
+  return _team;
 };
 
-const registerUser = async () => {
-  const [user, team] = await Promise.all([User.findOne({ email }), Team.findById(teamId)]);
+const registerUser = async (email, teamId) => {
+  const [user, team] = await Promise.all([
+    User.findOne({ email }, ""),
+    Team.findById(teamId)
+  ]);
 
-  if (!user) { return "invalid user" }
+  if (!user) { return }
 
-  const isTeamMember = team.members.some((member) => member === user["_id"]);
-  const isMyTeam = user.teams.some((team) => team === team["_id"]);
+  const isTeamMember = team.members.some((memberOid) => {
+    return memberOid.toString() === user["_id"].toString();
+  });
+  const isMyTeam = user.teams.some((teamOid) => {
+    return teamOid.toString() === team["_id"].toString();
+  });
 
-  if (!isTeamMember && !isMyTeam) {
-    team.members.push(user);
-    user.teams.push(team);
+  if (isTeamMember || isMyTeam) { return }
 
-    await Promise.all([team.save(), user.save()]);
-  }
+  team.members.push(user);
+  user.teams.push(team);
 
-  return "register user";
+  await Promise.all([team.save(), user.save()]);
+
+  return user;
 };
 
-module.exports = { createTeam, registerUser };
+module.exports = { createTeam, getTeams, registerUser };
--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -1,16 +1,13 @@
 const aws = require("aws-sdk");
-const express = require("express");
 const createError = require("http-errors");
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-const jwt = require("jsonwebtoken");
 
-const Location = require("../models/Location");
 const Team = require("../models/Team");
-const User = require("../models/User");
-const Sports = require("../models/Sports");
 
-const { createTeam, registerUser } = require("../models/controllers/teamController");
+const { createTeam, getTeams, registerUser } = require("../models/controllers/teamController");
 const { getMyTeams } = require("../models/controllers/userController");
 
 require("dotenv").config();
@@ -28,41 +25,61 @@ const upload = multer({
     s3,
     acl: "public-read",
     bucket: "minho-bucket",
-    metadata: function (req, file, cb) {
+    metadata(req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
-    key: function (req, file, cb) {
+    key(req, file, cb) {
       cb(null, Date.now().toString());
-    }
+    },
   }),
 });
 
-// router.get("/my-team/:myTeamId", async (req, res, next) => {
-//   try {
-//     const { myTeamId } = req.params;
-//     const team = await Team.findById({_id: myTeamId })
-//       .populate("captin", "name email")
-//       .populate("members", "name email")
-//       .populate("location")
-//       .populate({ path: "matches", populate: { path: "teams", select: "name" } })
-//       .populate({ path: "matches", populate: { path: "playground", select: "name address" }});
+router.get("/", async (req, res, next) => {
+  try {
+    const { province, city, district, sports } = req.query;
 
-//     res.status(200).json({
-//       message: "success",
-//       data: team,
-//       error: null,
-//     });
-//   } catch (err) {
-//     res.status(500).json({
-//       message: "fail",
-//       data: null,
-//       error: "error",
-//     });
+    const teams = await getTeams(province, city, district, sports);
 
-//     console.log(`GET : /team/my-team/:myTeamId - ${err}`);
-//     next(createError(500, "Internal Server Error"));
-//   }
-// });
+    res.status(200).json({
+      message: "success",
+      data: teams,
+      error: null,
+    });
+  } catch (err) {
+    res.status(200).json({
+      message: "fail",
+      data: null,
+      error: "error",
+    });
+
+    console.log(`GET : /team - ${err}`);
+    next(createError(500, "Internal Server Error"));
+  }
+});
+
+router.get("/my", async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const { email } = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+
+    const teams = await getMyTeams(email);
+
+    res.status(200).json({
+      message: "success",
+      data: teams,
+      error: null,
+    });
+  } catch (err) {
+    res.status(200).json({
+      message: "fail",
+      data: null,
+      error: "error",
+    });
+
+    console.log(`GET : /team/my - ${err}`);
+    next(createError(500, "Internal Server Error"));
+  }
+});
 
 router.post("/", async (req, res, next) => {
   try {
@@ -120,37 +137,13 @@ router.post("/emblem", upload.single("image"), (req, res, next) => {
   }
 });
 
-router.get("/my", async (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-    const { email } = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
-
-    const teams = await getMyTeams(email);
-
-    res.status(200).json({
-      message: "success",
-      data: teams,
-      error: null,
-    });
-  } catch (err) {
-    res.status(200).json({
-      message: "fail",
-      data: null,
-      error: "error",
-    });
-
-    console.log(`GET : /team/my - ${err}`);
-    next(createError(500, "Internal Server Error"));
-  }
-});
-
 router.patch("/members", async (req, res, next) => {
   try {
     const { email, teamId } = req.body;
-    const result = await registerUser(email, teamId);
+    const user = await registerUser(email, teamId);
 
-    if (result === "invalid user") {
-      res.status(200).json({
+    if (!user) {
+      return res.status(200).json({
         message: "invalid user",
         data: null,
         error: null,
@@ -170,77 +163,6 @@ router.patch("/members", async (req, res, next) => {
     });
 
     console.log(`PATCH : /team/members - ${err}`);
-    next(createError(500, "Internal Server Error"));
-  }
-});
-
-router.get("/", async (req, res, next) => {
-  try {
-    const { province, city, district, sports } = req.query;
-
-    const [location, _sports] = await Promise.all([
-      await Location.findOne({ province, city, district }),
-      await Sports.findOne({ sports }),
-    ]);
-
-    const locationOid = location["_id"];
-    const sportsOid = _sports["_id"];
-
-    const teams = await Team.find({
-      $and: [{ location: locationOid }, { sports: sportsOid }]
-    }).populate("location")
-      .populate("caption")
-      .populate("sports")
-      .populate("members")
-      .populate({ path: "matches", populate: { path: "playground" } })
-      .populate({ path: "matches", populate: { path: "teams" } });
-
-    const _teams = teams.map((team) => {
-      const matches = team.matches.map((match) => {
-        const { playtime, playground } = match;
-
-        return {
-          start: playtime.start,
-          end: playtime.end,
-          playgroundName: playground.name,
-          province: playground.address.province,
-          city: playground.address.city,
-          district: playground.address.district,
-          teams: match.teams[1]
-            ? [match.teams[0].name, match.teams[1].name]
-            : [match.teams[0].name],
-        };
-      });
-
-      return {
-        id: team["_id"],
-        name: team.name,
-        captin: team.captin.name,
-        province: team.location.province,
-        city: team.location.city,
-        district: team.location.district,
-        emblem: team.emblem,
-        members: team.members,
-        manner: team.repute.manner,
-        ability: team.repute.ability,
-        matches: matches,
-        rank: team.rank,
-      };
-    });
-
-    res.status(200).json({
-      message: "success",
-      data: _teams,
-      error: null,
-    });
-  } catch (err) {
-    res.status(200).json({
-      message: "fail",
-      data: null,
-      error: "error",
-    });
-
-    console.log(`GET : /team - ${err}`);
     next(createError(500, "Internal Server Error"));
   }
 });
